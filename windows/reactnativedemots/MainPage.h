c1131742d972a3143d31ea90c8f630c88a4d9626
sakurafisch <sakurafisch@gmail.com>
2021-10-10 16:04:03 +0800

    Initial commit

---
+++ windows/reactnativedemots/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::reactnativedemots::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::reactnativedemots::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
